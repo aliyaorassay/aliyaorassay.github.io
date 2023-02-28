bf82461c5147de71a2cd80d1b94de44eb18993f3
aliyaorassay <aliya.orassay@nu.edu.kz>
2023-02-28 21:22:31 +0600

    adding lab6

---
+++ LAB6/lab6.js
@@ -0,0 +1,49 @@
+
+    function exercise1(){
+        const r=3;
+        const p=3.14;
+        const Area=p*r*r;
+        const result={
+            Area_of_circle: Area,
+    
+           }
+            console.log ("Exercise 1", result)
+        }
+       
+    function exercise2 () {
+        const a=1;
+        const lowest=0;
+        const highest=1;
+     
+
+        if (a>=lowest&&a<=highest) {
+            console.log ('Exercise 2. Check if the numbers lay between 0 and 1: True, the number between 0 and 1');
+            }
+            else {
+                console.log (" Exercise 2. Check if the numbers lay between 0 and 1: False, the number is not between 0 and 1");
+            }
+        }
+    
+       
+
+    function exercise3 () {
+        const b=1;
+        const c=1; 
+            if (b==c){
+                console.log ("Exercise 3. Check if numbers are equal: Equal");
+            }
+                else {
+                    console.log ("Exercise 3. Check if numbers are equal: Not equal");
+                }
+    } 
+        
+ 
+
+
+
+    
+
+
+
+
+
